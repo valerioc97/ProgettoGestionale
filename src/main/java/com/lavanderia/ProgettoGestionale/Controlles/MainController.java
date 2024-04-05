638a2eccbecc5e36fadafd543d81ebb02dddc938
Valerio Carcaterra <valerio.carcaterra@outlook.com>
2024-04-05 15:13:12 +0200

    Aggiunta soluzione di stampa per cloud

--- src/main/java/com/lavanderia/ProgettoGestionale/Controlles/MainController.java
+++ src/main/java/com/lavanderia/ProgettoGestionale/Controlles/MainController.java
@@ -6,6 +6,7 @@ import com.lavanderia.ProgettoGestionale.dtos.ClienteDto;
 import com.lavanderia.ProgettoGestionale.models.Capi;
 import com.lavanderia.ProgettoGestionale.models.Cliente;
 import com.lavanderia.ProgettoGestionale.services.ClienteService;
+import com.lavanderia.ProgettoGestionale.services.PrinterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,8 @@ public class MainController {
 
     @Autowired
     private ClienteService clienteService;
+    @Autowired
+    private PrinterService printerService;
 
     @GetMapping("/clienti")
     public ResponseEntity<List<Cliente>> getClienti(){
@@ -98,6 +101,7 @@ public class MainController {
         return ResponseEntity.ok(res);
     }
 
+    //Soluzione on-premise.
     @GetMapping("/print")
     public ResponseEntity<String> stampaStringa(){
         String contentToPrint = "Contenuto da stampare";
@@ -117,6 +121,22 @@ public class MainController {
             job.print();
             messaggio = "Contenuto stampato";
         } catch (PrinterException e) {
+            messaggio = "Contenuto non stampato";
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(messaggio);
+    }
+
+    //Soluzione cloud/remoto
+    @GetMapping("/print1")
+    public ResponseEntity<String> stampaStringa1(){
+        String contentToPrint = "Contenuto da stampare";
+        String messaggio;
+
+        try {
+            printerService.print(contentToPrint);
+            messaggio = "Contenuto stampato";
+        } catch (Exception e) {
             messaggio = "Contenuto non stampato";
             throw new RuntimeException(e);
         }
