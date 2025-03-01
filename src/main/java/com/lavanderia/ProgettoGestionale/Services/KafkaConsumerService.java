fa560190870bb5c14212ea792c35cfd8ee08f1f0
Valerio Carcaterra <v.carcaterra.esterno@almaviva.it>
2025-03-01 04:29:03 +0100

    Aggiunto kafka

---
+++ src/main/java/com/lavanderia/ProgettoGestionale/Services/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+package com.lavanderia.ProgettoGestionale.Services;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "test-topic", groupId = "my-consumer-group")
+    public void consume(String message){
+        System.out.println("Received message: " + message);
+    }
+}
